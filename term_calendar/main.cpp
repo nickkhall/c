bc1fc1bf49f57d5b7ee92a2fa62455a0032c8364
nickkhall <nick.hall@bluelogicteam.com>
2018-07-22 20:57:01 -0500

    wip, clean up / making script more efficient

--- term_calendar/main.cpp
+++ term_calendar/main.cpp
@@ -11,6 +11,9 @@ struct MonthData
 {
   time_t CurrentTime { time(0) };
   vector<string> MonthsArray { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+  vector<string> WeekdayArray { "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" };
+  string Calendar { "" };
+  // 20
 };
 
 string GetTime(time_t &CurrentTime)
@@ -27,9 +30,55 @@ string GetTime(time_t &CurrentTime)
   return TimeString;
 }
 
+string PrintWeekdays(MonthData &NewMonth)
+{
+  string Weekdays { "" };
+
+  for (int i = 0; i < 7; i++)
+  {
+    Weekdays += NewMonth.WeekdayArray.at(i);
+    if (i != 7)
+    {
+      Weekdays += " ";
+    }
+  }
+
+  return Weekdays;
+}
+
+string FormatString(string &calendar, int num_days, int CurrentDay)
+{
+  string formatted_string { "" };
+
+  cout << *calendar << endl;
+
+  // for (int d = 1; d <= num_days; d++)
+  // {
+  //   // if it's today, turn day red
+  //   if (d == CurrentDay) {
+  //     *calendar += "\033[1;31m";
+  //     *calendar += to_string(d);
+  //     *calendar += " \033[0m";
+  //   } else {
+  //     if (d <= 7)
+  //     {
+  //       *calendar += " ";
+  //     }
+  //     *calendar += to_string(d);
+  //     *calendar += " ";
+  //   }
+  //
+  //   // After 7 days, add a new line
+  //   if (d % 7 == 0)
+  //     *calendar += "\n";
+  // }
+
+  return formatted_string;
+}
+
 string GetCalendar(MonthData &NewMonth)
 {
-  string calendar { "" };
+  string *temp_calendar = &NewMonth.Calendar;
   vector<int> TEM { 2 };
   vector<int> TDM { 4, 6, 9, 11 };
   vector<int> TODM { 1, 3, 5, 7, 8, 10, 12 };
@@ -44,44 +93,20 @@ string GetCalendar(MonthData &NewMonth)
   calendar += "\n";
   calendar += "----------------------\n";
 
+  calendar += PrintWeekdays(NewMonth);
+  calendar += "\n";
+
   // Determine how many days is in current month
   if (find(TODM.begin(), TODM.end(), CurrentMonth) != TODM.end())
   {
     // 31 Day Month
-    for (int d = 1; d <= 31; d++)
-    {
-      // if it's today, turn day red
-      if (d == CurrentDay) {
-        calendar += "\033[1;31m ";
-        calendar += to_string(d);
-        calendar += " \033[0m";
-      } else {
-        calendar += to_string(d);
-        calendar += " ";
-      }
-
-      // After 7 days, add a new line
-      if (d % 7 == 0)
-        calendar += "\n";
-    }
+    FormatString(calendar, 31, CurrentDay);
     // 30 Day Month
   } else if (find(TDM.begin(), TDM.end(), CurrentMonth) != TDM.end()) {
-    for (int d = 1; d <= 30; d++)
-    {
-      calendar += to_string(d) += " ";
-      if (d % 7 == 0) {
-        calendar += "\n";
-      }
-    }
+    FormatString(calendar, 30, CurrentDay)
     // 28 Day Month
   } else {
-    for (int d = 1; d <= 28; d++)
-    {
-      calendar += to_string(d) += " ";
-      if (d % 7 == 0) {
-        calendar += "\n";
-      }
-    }
+    FormatString(calendar, 28, CurrentDay);
   }
 
   return calendar;
