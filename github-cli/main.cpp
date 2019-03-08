ea6ca021829280b9984f8721bed916689919304e
nickhallph <nick.hall@yum.com>
2019-03-07 23:50:55 -0600

    removed commented out menu

--- github-cli/main.cpp
+++ github-cli/main.cpp
@@ -40,52 +40,52 @@ int main(int argc, char ** argv) {
   // Enable arrow keys
   keypad(window, true);
 
-  // string choices[3] = {"Create Repository", "Update Repository", "Clone Repository"};
-  // int choice;
-  // int highLight = 0;
-  //
-  // while(1) {
-  //   for(int c = 0; c < 3; c++) {
-  //     // For selected option, turn on attribute reverse,
-  //     // which reverses the color scheme. (white bg, black text)
-  //     if(c == highLight) {
-  //       wattron(window, A_REVERSE);
-  //     }
-  //
-  //     // Regardless, move to x,y coord and print option
-  //     mvwprintw(window, c + 2, 10, choices[c].c_str());
-  //     // Turn off attribute.
-  //     wattroff(window, A_REVERSE);
-  //   }
-  //
-  //   choice = wgetch(window);
-  //
-  //
-  //   switch(choice) {
-  //     case KEY_UP:
-  //       highLight--;
-  //       if (highLight == -1) {
-  //         highLight = 0;
-  //       }
-  //       break;
-  //     case KEY_DOWN:
-  //       highLight++;
-  //       if (highLight == 3) {
-  //         highLight = 2;
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //
-  //   // If user hits 'Enter' key
-  //   if(choice == 10) {
-  //     // Do things if user hits 'Enter'
-  //
-  //     // Break out of while loop
-  //     break;
-  //   }
-  // }
+  string choices[3] = {"Create Repository", "Update Repository", "Clone Repository"};
+  int choice;
+  int highLight = 0;
+
+  while(1) {
+    for(int c = 0; c < 3; c++) {
+      // For selected option, turn on attribute reverse,
+      // which reverses the color scheme. (white bg, black text)
+      if(c == highLight) {
+        wattron(window, A_REVERSE);
+      }
+
+      // Regardless, move to x,y coord and print option
+      mvwprintw(window, c + 2, 10, choices[c].c_str());
+      // Turn off attribute.
+      wattroff(window, A_REVERSE);
+    }
+
+    choice = wgetch(window);
+
+
+    switch(choice) {
+      case KEY_UP:
+        highLight--;
+        if (highLight == -1) {
+          highLight = 0;
+        }
+        break;
+      case KEY_DOWN:
+        highLight++;
+        if (highLight == 3) {
+          highLight = 2;
+        }
+        break;
+      default:
+        break;
+    }
+
+    // If user hits 'Enter' key
+    if(choice == 10) {
+      // Do things if user hits 'Enter'
+
+      // Break out of while loop
+      break;
+    }
+  }
 
   getch();
 
