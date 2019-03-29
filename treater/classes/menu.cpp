fbd29d88fa7893ffcda883ea540c07fd6ce18487
nickhallph <nick.hall@yum.com>
2019-03-29 12:12:56 -0500

    removed unnecessary var && added more comments to menu.cpp

--- treater/classes/menu.cpp
+++ treater/classes/menu.cpp
@@ -66,17 +66,18 @@ int Menu::PrintMenu(Window* window, int yDividend, int xDividend) {
     refresh();
     wrefresh(window->windowInstance);
 
+    // Iterate over current list to render
     for (int i = 0; i < itemsContainer[pageNum].size(); i++) {
+      // If the current item in list matches the number of highlighted
       if (i == highlighted) {
+        // Switch the colors (white bg, black text)
         wattron(window->windowInstance, A_REVERSE);
       }
-
-      int curY = window->yMax / y + (i + 2);
 
       // Move to y, x coordinates and print current item
       mvwprintw(
         window->windowInstance,
-        curY,
+        window->yMax / y + (i + 2),
         window->xMax / x - (strlen(itemsContainer[pageNum][i].c_str()) / 2),
         itemsContainer[pageNum][i].c_str()
       );
