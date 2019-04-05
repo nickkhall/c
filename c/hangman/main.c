a8d23ca75fd7e9fcba529da33c7b1619794bdf0f
nickkhall <nick.hall@bluelogicteam.com>
2019-04-04 22:18:50 -0500

    wip, changed things

--- c/hangman/main.c
+++ c/hangman/main.c
@@ -1,33 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define LENGTH 500
 
-struct word {
-  char name[LENGTH];
-};
+void printStatus(size_t wordCount, unsigned int guess) {
+  system("clear");
+ 
+  printf("Welcome to hangman.\nEvery word will be a car manufacture and model.\nThe current word is %zu characters long.\n\nGuess: %d\n\nPlease guess a character\n\nCharacter: ", wordCount, guess);
+}
 
-void populateWordsList(FILE* file, struct word* w) {
-  int i = 0;
-
-  fscanf(file, "%c", &w->name[i]);
-
-  while(w->name[i] != EOF && i < LENGTH) {
-    i++;
-    fscanf(file, "%c", &w->name[i]);
-  }
+void getUserChar(char* c, unsigned int* guess) {
+  *c = getchar();
+  *guess = *guess + 1;
 }
 
 int main() {
-  struct word words;
+  char words[50][500];
   FILE* wordlist = fopen("words.txt", "r");
-  
-  populateWordsList(wordlist, &words);
+  int i = 0;
+  int total = 0;
+  int guess = 0;
+
+  while(fgets(words[i], 500, wordlist)) {
+    words[i][strlen(words[i]) - 1] = '\0';
+    i++;
+  }
+
+  total = i;
   
   fclose(wordlist);
 
-  printf("%s", words.name);
- 
+  const char* randomWord;
+
+  srand((unsigned)time(NULL));
+  int randomNumber = rand() % 7;
+
+  randomWord = words[randomNumber];
+  size_t wordCount = strlen(randomWord);
+
+  char c;
+  
+  while(guess <= (int)wordCount) {
+    printStatus(wordCount, guess);
+    getUserChar(&c, &guess);
+  }
+
   return 0;
 };
