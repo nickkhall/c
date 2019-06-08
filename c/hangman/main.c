5f71cd76bddbb318cb914d82f2f50f5be201d46e
nickhallph <nick.hall@yum.com>
2019-06-08 00:14:22 -0500

    probably over complicating things but im trying to be efficient and learn

--- c/hangman/main.c
+++ c/hangman/main.c
@@ -13,23 +13,83 @@ void printHeader() {
   printf("|--------------------------------------------------------------------|\n");
 }
 
-void printHumanStatus() {
+void printHumanStatus(int guess) {
+  int body[7];
+
+  for (int b = 0; b < 7; b++) {
+    body[b] = 32;
+  }
+
+  switch(guess) {
+    case 0:
+      body[6] = 92;
+      break;
+    case 1:
+      body[5] = 47;
+      break;
+    case 2:
+      body[4] = 179;
+      break;
+    case 3:
+      body[3] = 92;
+      break;
+    case 4:
+      body[2] = 47;
+      break;
+    case 5:
+      body[1] = 179;
+      break;
+    case 6:
+      body[0] = 79;
+      break;
+    default:
+      break;
+  }
+
+  if (guess == 4) body[guess] = 79;
+  else if (guess % 2 == 0) {
+    body[guess] = 47;
+    body[guess + 1] = 92;
+  }
+  else body[guess] = 179;
+
   printf("|                                |                                   |\n");
-  printf("|                                O                                   |\n");
-  printf("|                               / \\                                  |\n");
-  printf("|                                |                                   |\n");
-  printf("|                               / \\                                  |\n");
+  printf("|                                %c                                   |\n", body[0]);
+  printf("|                               %c %c                                  |\n", body[1], body[2]);
+  printf("|                                %c                                   |\n", body[3]);
+  printf("|                               %c %c                                  |\n", body[4], body[5]);
   printf("|                     +--------------------+                         |\n");
-  printf("|                    /                    \/|                         |\n");
-  printf("|                   /____________________\/ |                         |\n");
+  printf("|                    /                    /|                         |\n");
+  printf("|                   /____________________/ |                         |\n");
   printf("|                  |                     |                           |\n");
   printf("|                  |                     |                           |\n");
   printf("|                                                                    |\n");
   printf("+--------------------------------------------------------------------+\n");
 }
 
-void printGameStatus() {
-  printf("|  Score: %d        Word count: %d       Guesses Left: %d  |\n");
+void printGameStatus(int score, int guess, int wordCount) {
+  int counter = 0;
+  // 70 full
+  // 68 without border
+
+  for (int i = 0; i < score; i++) {
+    if (i == 10 || i == 100 || i == 1000 || i == 10000) {
+      counter += 1;
+    }
+  }
+  counter = 0;
+
+  for (int x = 0; x < 68; x++) {
+    if (i < 7 + counter) {
+      printf("Score: %d", score);
+    } else if (i > 7 + counter && i < ) {
+      printf("Guess: %d", guess);
+    } else if () {
+      printf("Word count: %d", wordCount);
+    } else {
+      printf("%c", 32);
+    }
+  }
   printf("+--------------------------------------------------------------------+\n");
 }
 
@@ -37,12 +97,12 @@ void printGuess() {
   printf("Guess: ");
 }
 
-void printStatus(size_t wordCount, unsigned int guess) {
+void printStatus(unsigned int score, unsigned int guess, unsigned int wordCount) {
   system("clear");
 
   printHeader();
-  printHumanStatus();
-  printGameStatus();
+  printHumanStatus(guess);
+  printGameStatus(score, guess, wordCount);
 }
 
 void getUserChar(char* c, unsigned int* guess) {
@@ -54,8 +114,9 @@ int main() {
   char words[50][500];
   FILE* wordlist = fopen("words.txt", "r");
   int i = 0;
+  int score = 0;
   int total = 0;
-  int guess = 0;
+  int guess = 7;
 
   while(fgets(words[i], 500, wordlist)) {
     words[i][strlen(words[i]) - 1] = '\0';
@@ -77,7 +138,7 @@ int main() {
   char c;
 
   while(guess <= (int)wordCount) {
-    printStatus(wordCount, guess);
+    printStatus(score, guess, wordCount);
     printGuess();
     getUserChar(&c, &guess);
   }
