2898bf031da8897cae4f67522401a7c476a3e84d
nickhallph <nick.hall@yum.com>
2019-07-07 18:12:35 -0500

    wip

--- cpp/employee-manager/classes/form.cpp
+++ cpp/employee-manager/classes/form.cpp
@@ -1,38 +1,22 @@
 #include <ncurses.h>
-#include <iostream>
-#include <cstring>
+#include <vector>
 
 #include "../headers/form.hpp"
 #include "../headers/employee.hpp"
 
 Form::Form() {};
 
-void Form::CollectInfo() {
+void Form::Create() {
   int key_code {0};
 
   ClearScreen();
 
   curs_set(1);
 
-  do {
-    mvwprintw(window, y_max / 2, (x_max / 2) - 4, "test");
+  for (int i = 0; i < form_items.size(); i++) {
+    mvwprintw(window, y_max / 2, (x_max / 2) - 4, form_items[i].c_str());
+  }
     
-    refresh();
-    wrefresh(window);
-  } while (key_code != 27 || key_code != 10);
 };
-
-// void Form::Create(const unsigned int &type) {
-//   curs_set(1);
-// 
-//   switch(type) {
-//     // Employee type
-//     case 0: {
-//        Employee NewEmployee;
-//        NewEmployee = NewEmployee.CreateEmployee();
-//     }
-//   };
-// 
-// };
 
 Form::~Form() {};
