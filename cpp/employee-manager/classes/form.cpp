3a6412cabf6c25d765a402e1189fbbf42b05d61f
nickkhall <nick.hall@bluelogicteam.com>
2019-07-14 22:11:34 -0500

    wip

--- cpp/employee-manager/classes/form.cpp
+++ cpp/employee-manager/classes/form.cpp
@@ -10,22 +10,22 @@ int Form::Create(const std::vector<std::string> &form_items) {
   int key_code {0};
 
   curs_set(1);
+	echo();
 	unsigned int cur_item {0};
-	std::vector<char*> inputs {};
-		 
+
   do {
     ClearScreen();
-    mvwprintw(window, y_max / 2, x_max - (19 / 2), "(100 character max)");
+    mvwprintw(window, y_max / 2, (x_max / 2) - (19 / 2), "(100 character max)");
 
-    char *input = (char*)malloc(sizeof(char) * 100);
+    char *input = (char*)malloc(sizeof(char) * 400);
 
     // Print out input text
-    mvwprintw(window, y_max, x_max - (8 + form_items[cur_item].length()), ("Enter " + form_items[cur_item] + ":  ").c_str());
+    mvwprintw(window, y_max, (x_max / 2) - (8 + form_items[cur_item].length()), ("Enter " + form_items[cur_item] + ":  ").c_str());
     refresh();
     wrefresh(window);
 
     // Get user input
-    mvwgetnstr(window, y_max, x_max, input, 100);
+    mvwgetnstr(window, y_max, x_max, input, 400);
     refresh();
     wrefresh(window);
 
@@ -33,6 +33,7 @@ int Form::Create(const std::vector<std::string> &form_items) {
     cur_item++;
   } while(cur_item < form_items.size());
 
+	curs_set(0);
 	return 0;
 };
 
