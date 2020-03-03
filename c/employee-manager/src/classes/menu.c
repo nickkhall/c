eea12fa981a378ea270a574e155a15143b0d532f
nickkhall <nick.hall@bluelogicteam.com>
2020-03-03 12:14:46 -0600

    wip, fixed menu frame, need bottom

--- c/employee-manager/src/classes/menu.c
+++ c/employee-manager/src/classes/menu.c
@@ -21,23 +21,13 @@ struct coords* get_screen_size() {
 
 void print_menu(const char menu_items[16][4], const int row, const int col) {	
 	for (int y = 0; y < row - 2; y++) {
-		if (y == 0) {
-			printf("+");
-		}
-
-		for (int x = 0; x < col - 1; x++) {
-			// Print corners
+		for (int x = 0; x < col; x++) {
 			if (y == 0) {
-				if (x == col - 2 || y == row - 3) {
-					printf("+");
-				} else printf("-");
-			} else if ((y == 0 && x == col - 1) || (y == row - 3 && x == col - 2)) {
-				printf("+");
-			} else if (x == 0 || x == col - 3) {
-				printf("¦");
-			} else {
-				printf(" ");
-			}
+				if (x == 0 || x == col - 1) printf("+");
+				else printf("-");
+			} else if (y > 0 && y < row - 2 && (x == 0 || x == col - 1)) {
+				printf("|");
+			} else printf(" ");
 		}
 	}
 };
