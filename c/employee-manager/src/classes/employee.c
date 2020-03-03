ee59815bb329ec6ce5ba76316a79afe493fbd24f
nickhallph <nick.hall@yum.com>
2020-03-02 20:00:44 -0600

    stable init

--- c/employee-manager/src/classes/employee.c
+++ c/employee-manager/src/classes/employee.c
@@ -1,39 +1,5 @@
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "../headers/employee.h"
-
-char* create_uuid() {
-	int min = 0;
-	int max = 30;
-  char* uid = NULL;
-	uid = (char*) malloc(sizeof(max));
-
-  char chars[35] = {
-		'0', '1', '2', '3',
-    'p', 'b', 'k', 'd',
-    'v', 'w', 'x', 'y',
-		'4', '5', 'z',
-    'e', 'f', 'g', 'h',
-    'r', 's', 't', 'u',
-    'i', 'j', 'c', 'l',
-		'8', '9', 'o', 'a',
-    'm', 'n', '6', '7',
-  };
-
-  // Set random seed
-  srand(time(0));
-
-  for (int i = 0; i < max; i++) {
-    int num = (rand() % (min - max + 1)) + min;
-
-		if (i != 0 && (i+1) % 5 == 0) uid[i] = '-';
-		else uid[i] = chars[num];
-  };
-
-  return uid;
-};
+#include "./utils.c"
 
 Employee create_employee(
 	int age,
--- c/employee-manager/src/main.c
+++ c/employee-manager/src/main.c
@@ -1,6 +1,6 @@
-#import <string.h>
-#import <stdio.h>
-#import "headers/employee.h"
+#include <string.h>
+#include <stdio.h>
+#include "headers/employee.h"
 
 int main() {
 	Employee test_employee = create_employee(
@@ -10,16 +10,6 @@ int main() {
 		"000-111-2222",
 		"test@foo.bar",
 		'm'
-	);
-
-	printf(
-		"%s %s is approx %d year(s) old. You can reach %s at %s, or at %s.\n",
-		test_employee.first,
-		test_employee.last,
-		test_employee.age,
-		((test_employee.gender == 0x6d || test_employee.gender == 0x4d) ? "him" : "her"),
-		test_employee.phone,
-		test_employee.email
 	);
 
   return 0;
